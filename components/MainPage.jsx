299f3f0b70b42f715132a80b6edd3020a3d4167c
Catur Hidayat <caturhidayat00@gmail.com>
2023-07-03 19:30:12 +0700

    style: views topics as stack

--- components/MainPage.jsx
+++ components/MainPage.jsx
@@ -25,6 +25,9 @@ import {
     SiGnubash,
     SiHtml5,
     SiChakraui,
+    SiPrisma,
+    SiBulma,
+    SiDocker,
 } from "react-icons/si";
 import { TbBrandNextjs } from "react-icons/tb";
 import { BiLogoPostgresql } from "react-icons/bi";
@@ -63,77 +66,104 @@ export default function MainPage({ repos }) {
                     <CardFooter>
                         <Stack justifyItems='start'>
                             <HStack>
-                                <Text fontSize='sm' fontWeight='semibold'>
+                                <Text fontSize='xl' fontWeight='normal'>
                                     Stack:
                                 </Text>
+                                {value.topics.map((topic) => (
+                                    <HStack key={topic} >
+                                        {topic === "javaScript" && (
+                                            <HStack>
+                                                <SiJavascript />
+                                            </HStack>
+                                        )}
+                                        {topic === "typescript" && (
+                                            <HStack>
+                                                <SiTypescript />
+                                            </HStack>
+                                        )}
+                                        {topic === "css" && (
+                                            <HStack>
+                                                <SiCss3 />
+                                            </HStack>
+                                        )}
+                                        {topic === "pug" && (
+                                            <HStack>
+                                                <SiPug />
+                                            </HStack>
+                                        )}
 
-                                {value.language === "JavaScript" && (
-                                    <HStack>
-                                        <SiJavascript />
+                                        {topic === "shell" && (
+                                            <HStack>
+                                                <SiGnubash />
+                                            </HStack>
+                                        )}
+                                        {topic === "docker" && (
+                                            <HStack>
+                                                <SiDocker />
+                                            </HStack>
+                                        )}
+                                        {topic === "python" && (
+                                            <HStack>
+                                                <SiPython />
+                                            </HStack>
+                                        )}
+                                        {topic === "html" && (
+                                            <HStack>
+                                                <SiHtml5 />
+                                            </HStack>
+                                        )}
+                                        {topic === "handlebars" && (
+                                            <HStack>
+                                                <SiHandlebarsdotjs />
+                                            </HStack>
+                                        )}
+                                        {topic === "nodejs" && (
+                                            <HStack>
+                                                <SiNodedotjs />
+                                            </HStack>
+                                        )}
+                                        {topic === "expressjs" && (
+                                            <HStack>
+                                                <SiExpress />
+                                            </HStack>
+                                        )}
+                                        {topic === "nextjs" && (
+                                            <HStack>
+                                                <TbBrandNextjs />
+                                            </HStack>
+                                        )}
+                                        {topic === "postgresql" && (
+                                            <HStack>
+                                                <BiLogoPostgresql />
+                                            </HStack>
+                                        )}
+                                        {topic === "prisma" && (
+                                            <HStack>
+                                                <SiPrisma />
+                                            </HStack>
+                                        )}
+                                        {topic === "chakra-ui" && (
+                                            <HStack>
+                                                <SiChakraui />
+                                            </HStack>
+                                        )}
+                                        {topic === "bulma-css" && (
+                                            <HStack>
+                                                <SiBulma />
+                                            </HStack>
+                                        )}
+                                        {!topic && (
+                                            <HStack>
+                                                <Tag
+                                                    size='sm'
+                                                    colorScheme='orange'
+                                                >
+                                                    No Detected
+                                                </Tag>
+                                            </HStack>
+                                        )}
                                     </HStack>
-                                )}
-                                {value.language === "TypeScript" && (
-                                    <HStack>
-                                        <SiTypescript />
-                                    </HStack>
-                                )}
-                                {value.language === "CSS" && (
-                                    <HStack>
-                                        <SiCss3 />
-                                    </HStack>
-                                )}
-                                {value.language === "pug" && (
-                                    <HStack>
-                                        <SiPug />
-                                    </HStack>
-                                )}
-                                {value.language === "shell" && (
-                                    <HStack>
-                                        <SiGnubash />
-                                    </HStack>
-                                )}
-                                {value.language === "Python" && (
-                                    <HStack>
-                                        <SiPython />
-                                    </HStack>
-                                )}
-                                {value.language === "html" && (
-                                    <HStack>
-                                        <SiHtml5 />
-                                    </HStack>
-                                )}
-                                {value.language === "Handlebars" && (
-                                    <HStack>
-                                        <SiHandlebarsdotjs />
-                                    </HStack>
-                                )}
-                                {value.topics === "nextjs" && (
-                                    <HStack>
-                                        <TbBrandNextjs />
-                                    </HStack>
-                                )}
-                                {value.language === "expressjs" && (
-                                    <HStack>
-                                        <SiExpress />
-                                    </HStack>
-                                )}
-                                {value.language === "chakra-ui" && (
-                                    <HStack>
-                                        <SiChakraui />
-                                    </HStack>
-                                )}
-                                {value.language === "postgresql" && (
-                                    <HStack>
-                                        <BiLogoPostgresql />
-                                    </HStack>
-                                )}
-                                {!value.language && (
-                                    <HStack>
-                                        <Tag size='sm' colorScheme='orange'>
-                                            No Detected
-                                        </Tag>
-                                    </HStack>
-                                )}
+                                ))}
                             </HStack>
                             <HStack justifyItems='start' justifyContent='start'>
                                 <Text fontSize='sm' fontWeight='semibold'>
